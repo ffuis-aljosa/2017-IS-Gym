50e4ec7eef009bc359bcdae1125f8a73ac5ea773
Yellenuhh <33873676+Yellenuhh@users.noreply.github.com>
2018-06-01 16:31:24 +0200

    Delete tbl_login.sql

--- tbl_login.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Table] (
-    [loginId]  INT          NOT NULL,
-    [username] VARCHAR (50) NULL,
-    [password] VARCHAR (50) NULL,
-    PRIMARY KEY CLUSTERED ([loginId] ASC)
-);
-
